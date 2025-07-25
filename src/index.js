248cbed1a5dec099137d05626382982c2d7bea89
kapindo <kapindo@gmail.com>
2025-07-26 03:40:00 +0700

    link module to entry index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles/main.css";
+import TodoController from "./modules/TodoController.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const list = document.querySelector("ul");
+  new TodoController(form, list);
+})
+
